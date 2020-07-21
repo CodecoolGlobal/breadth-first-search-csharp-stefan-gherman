26dc702b67b52473fb5eaf4a2fd3606cc39d9488
Stefan Gherman <scgheman@gmail.com>
2020-07-22 02:42:03 +0300

    Add solution for first assignment

--- BFS-c-sharp/Program.cs
+++ BFS-c-sharp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using BFS_c_sharp.Model;
 using System;
 using System.Collections.Generic;
+using BFS_c_sharp;
 
 namespace BFS_c_sharp
 {
@@ -11,9 +12,37 @@ namespace BFS_c_sharp
             RandomDataGenerator generator = new RandomDataGenerator();
             List<UserNode> users = generator.Generate();
 
+            Console.WriteLine($"Number of users: {users.Count}");
             foreach (var user in users)
             {
-                Console.WriteLine(user);
+                Console.WriteLine($"{user} has the following friends: ");
+                foreach ( var friend in user.Friends)
+                {
+                    Console.Write($"{friend};");
+                }
+                Console.WriteLine('\n');
+            }
+            Console.WriteLine("-------------------------\n");
+            foreach ( var user in users)
+            {
+                Console.WriteLine($"Current user: {user}\n");
+                foreach (var targetUser in users)
+                {
+                    int pathLength = BFS.BFS.FindPath(users, user, targetUser);
+                    switch(pathLength)
+                    {
+                        case 0:
+                            Console.WriteLine($"Path from node {user} to {targetUser} is {pathLength}, same node");
+                            break;
+                        case -1:
+                            Console.WriteLine($"No path between {user} and {targetUser}");
+                            break;
+                        default:
+                            Console.WriteLine($"Path from node {user} to {targetUser} is {pathLength}");
+                            break;
+                    }
+                }
+                Console.WriteLine();
             }
 
             Console.WriteLine("Done");
