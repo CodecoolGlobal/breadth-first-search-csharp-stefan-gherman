c916f56b7bd2287d5e6e189442a1eeef67fec9a6
Stefan Gherman <scgheman@gmail.com>
2020-07-22 18:13:57 +0300

    Add method to get all shortest paths from one node to another

--- BFS-c-sharp/BFS/BFS.cs
+++ BFS-c-sharp/BFS/BFS.cs
@@ -34,11 +34,11 @@ namespace BFS_c_sharp.BFS
             while (searchQueue.Count != 0)
             {
                 var currentFirst = searchQueue.Dequeue();
-               
+
 
                 foreach (var friend in currentFirst.Friends)
                 {
-                    
+
                     if (friend.Equals(end))
                     {
                         return nodePerLevel[currentFirst] + 1;
@@ -55,14 +55,14 @@ namespace BFS_c_sharp.BFS
 
                     }
                 }
-                
-                
+
+
             }
 
             return -1;
         }
-        
-        public static HashSet<UserNode> FriendsOfFriendsAtDistance (List<UserNode> graph, UserNode target, int distance)
+
+        public static HashSet<UserNode> FriendsOfFriendsAtDistance(List<UserNode> graph, UserNode target, int distance)
         {
             HashSet<UserNode> friendsOfFriends = new HashSet<UserNode>();
             if (distance == 0)
@@ -72,7 +72,7 @@ namespace BFS_c_sharp.BFS
             }
 
             InitializeVisited(graph);
-            
+
 
             Queue<UserNode> searchQueue = new Queue<UserNode>();
 
@@ -93,14 +93,103 @@ namespace BFS_c_sharp.BFS
                         nodePerLevel.Add(friend, nodePerLevel[currentFirst] + 1);
                     }
 
-                    if(nodePerLevel[friend] == distance)
+                    if (nodePerLevel[friend] == distance)
                     {
                         friendsOfFriends.Add(friend);
                     }
                 }
             }
 
-                return friendsOfFriends;
+            return friendsOfFriends;
+        }
+
+        public static List<List<UserNode>> GetShortestPath(List<UserNode> graph, UserNode start, UserNode end)
+        {
+            List<List<UserNode>> allPaths = new List<List<UserNode>>();
+
+
+            if (start == end)
+            {
+                return new List<List<UserNode>>() { new List<UserNode>() { start } };
+            }
+
+            InitializeVisited(graph);
+            Queue<List<UserNode>> searchQueue = new Queue<List<UserNode>>();
+            searchQueue.Enqueue(new List<UserNode>() { start });
+            Queue<UserNode> searchQueueNodes = new Queue<UserNode>();
+            searchQueueNodes.Enqueue(start);
+            while (searchQueue.Count != 0)
+            {
+                var currentPath = searchQueue.Dequeue();
+                var lastNode = currentPath[currentPath.Count - 1];
+
+                if (lastNode.Equals(end))
+                {
+                    allPaths.Add(new List<UserNode>(currentPath));
+                }
+                else
+                {
+                    foreach (var friend in lastNode.Friends)
+                    {
+                        if (!searchQueueNodes.Contains(friend))
+                        {
+                            List<UserNode> addToPath = new List<UserNode>(currentPath);
+                            addToPath.Add(friend);
+                            searchQueue.Enqueue(addToPath);
+                            if (friend != end)
+                                searchQueueNodes.Enqueue(friend);
+                        }
+
+                    }
+                }
+            }
+            return allPaths.OrderBy(innerList => innerList.Count).
+                Where(innerList => innerList.Count == allPaths[0].Count).ToList();
+
+        }
+        public static List<List<UserNode>> GetAllPaths(List<UserNode> graph, UserNode start, UserNode end)
+        {
+            List<List<UserNode>> allPaths = new List<List<UserNode>>();
+
+
+            if (start == end)
+            {
+                return new List<List<UserNode>>() { new List<UserNode>() { start } };
+            }
+
+            InitializeVisited(graph);
+            Queue<List<UserNode>> searchQueue = new Queue<List<UserNode>>();
+            searchQueue.Enqueue(new List<UserNode>() { start });
+            Queue<UserNode> searchQueueNodes = new Queue<UserNode>();
+            searchQueueNodes.Enqueue(start);
+            while (searchQueue.Count != 0)
+            {
+                var currentPath = searchQueue.Dequeue();
+                var lastNode = currentPath[currentPath.Count - 1];
+
+                if (lastNode.Equals(end))
+                {
+                    allPaths.Add(new List<UserNode>(currentPath));
+                }
+                else
+                {
+                    foreach (var friend in lastNode.Friends)
+                    {
+                        if (!searchQueueNodes.Contains(friend))
+                        {
+                            List<UserNode> addToPath = new List<UserNode>(currentPath);
+                            addToPath.Add(friend);
+                            searchQueue.Enqueue(addToPath);
+                            if (friend != end)
+                                searchQueueNodes.Enqueue(friend);
+                        }
+
+                    }
+                }
+            }
+            return allPaths.OrderBy(innerList => innerList.Count).ToList();
+
+
         }
     }
 }
